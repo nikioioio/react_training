2e8ec05d5e12b30b823936dd0af75d55986d3a0d
Кедрун Никита <ked_nik@mail.ru>
2021-09-20 17:06:24 +0600

    работа с коллекциями

--- src/App.jsx
+++ src/App.jsx
@@ -3,75 +3,21 @@ import React from "react";
 
 class App extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         count: 0,
-    //         sameKey: false
-    //     };
-    // }
     state = {
-        count: 0,
-        isPause: true,
-        nameButtonStart: 'Start'
+        posts:[
+            {id: 'abc', name: 'JS Basis'},
+            {id: 'abc2', name: 'JS Adv'},
+            {id: 'abc3', name: 'ReactJs'},
+        ]
     }
 
 
-    // во время запуска
-    componentDidMount() {
-        if (localStorage.getItem('count')){
-            this.setState({count: parseInt(localStorage.getItem('count')),
-                                isPause: localStorage.getItem('isPause'),
-                                nameButtonStart: localStorage.getItem('nameButtonStart')})
-        }
-        console.log('componentDidMount');
-        this.initInterval();
-    }
-
-    // componentDidUpdate(prevProps, prevState, snapshot) {
-    //     console.log(prevState);
-    //
-    // }
-    //
-    // componentWillUnmount() {
-    //     console.log('componentWillUnmount')
-    // }
-
-    initInterval = () => {
-        this.timerId = setInterval(async () => {
-
-            if (!this.state.isPause) {
-                this.setState({count: this.state.count + 1});
-                localStorage.setItem('count', this.state.count);
-                localStorage.setItem('isPause', this.state.isPause);
-                localStorage.setItem('nameButtonStart', this.state.nameButtonStart);
-            }
-        }, 1000);
-    }
-
-    resumeInterval = () => {
-        this.state.isPause ?
-            this.setState({isPause: false, nameButtonStart: 'Stop'}):
-            this.setState({isPause: true, nameButtonStart: 'Start'})
-    }
-
-    clearInterval = () => {
-        localStorage.clear()
-        this.setState({count: 0, isPause: true, nameButtonStart: 'Start'})
-    }
 
 
     render() {
         return (
-            <div style={{margin: 'auto', width: '500px'}} className="App">
-                <h1>React Timer</h1>
-                <span style={mainStyle}>{this.state.count}</span>
-                <br/>
-                <div style={mainStyle1}>
-                    <button onClick={this.resumeInterval}>{this.state.nameButtonStart}</button>
-                    <button onClick={this.clearInterval}>reset</button>
-                </div>
-
+            <div className="App">
+                {this.state.posts.map(post => <h2 key={post.id}>{post.name}</h2>)}
             </div>
         );
     }
@@ -81,7 +27,3 @@ class App extends React.Component {
 
 export default App;
 
-
-const mainStyle = {position: 'absolute', left: '0', right: '0', margin: 'auto', width: '350px'}
-const mainStyle1 = {position: 'absolute', left: '0', right: '0', margin: 'auto', width: '430px'}
-// const countStyle = {position:'absolute'}
