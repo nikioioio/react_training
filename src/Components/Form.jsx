cb7129937af6d1f330a558ca1e1950cb09931687
Кедрун Никита <ked_nik@mail.ru>
2021-09-21 13:27:30 +0600

    работа с формами задача

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,37 +3,51 @@ import React from 'react';
 export class Form extends React.Component {
 
     state = {
-        firstName: '',
         email: '',
-        message: '',
-        select: '',
-        subscription: false,
-        gender: ''
+        agree: false,
     }
 
     handleChange = (event) => {
         this.setState({[event.target.name]: event.target.value})
     }
 
-    handleChecked = (event) => {
-        this.setState({subscription: event.target.checked})
+    handleCheck = (event) => {
+        this.setState({[event.target.name]: event.target.checked})
     }
 
-    valideteName = () => {
-        if (this.state.firstName.length < 12){
-            alert('Ошибка валидации firstname')
+    handleButton = () => {
+        if (!this.state.agree) {
+            alert('Вы не согласились с условиями!')
         }
-    }
 
-    validateEmail = () => {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!re.test(this.state.email)) {
+        if (!this.validateEmail(this.state.email)()){
             alert('Ошибка валидации email')
         }
+
+        if (this.state.agree && this.validateEmail(this.state.email)()) {
+            alert('Поздравляем с регистрацией!')
+            this.setState({email: '', agree: ''})
+        }
+
+
+    }
+
+
+    validateEmail = (str = '') => {
+        return () => {
+            const strValidate = str ? str : this.state.email;
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (!re.test(strValidate)) {
+                // alert('Ошибка валидации email')
+                return false
+            }
+            return true
+        }
+
     }
 
     render() {
-        const { firstName, email, message, select, subscription,gender } = this.state;
+        const {email, agree} = this.state;
 
         return (
             <div>
@@ -42,29 +56,20 @@ export class Form extends React.Component {
                        placeholder="email"
                        value={email}
                        onChange={this.handleChange}
-                       onBlur={this.validateEmail}
+                       onBlur={this.validateEmail(this.state.email)}
                 />
-                <br/>
-                <textarea name="message" value={message} onChange={this.handleChange}/>
-                <br />
-                <select name="select" value={select} onChange={this.handleChange}>
-                    <option value="" disabled></option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
 
                 <br/>
+
                 <input
                     type="checkbox"
-                    name="subscription"
-                    checked={subscription}
-                    onChange={this.handleChecked}
-                />
-                
+                    name="agree"
+                    checked={this.state.agree}
+                    onChange={this.handleCheck}
+                /> I agree with terms and conditions
+
                 <br/>
-                <input type="radio" name="gender" value="male" checked={this.state.gender === 'male'}  onChange={this.handleChange}/> Male
-                <input type="radio" name="gender" value="female" checked={this.state.gender === 'female'} onChange={this.handleChange}/> Female
+                <button onClick={this.handleButton}>Send</button>
 
             </div>
         )
