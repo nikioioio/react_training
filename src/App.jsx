fcabd552cd30fe33ee0abc6675ba397c64bad449
Кедрун Никита <ked_nik@mail.ru>
2021-09-22 21:31:46 +0600

    counter через hook usestate2

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React, {useState} from "react";
-
+import {Clicker} from './Clicker'
 
 const App = () => {
 
-    const [count,  setCount] = useState(0)
 
-    const handlerIncrement = () => {
-        setCount(count+1)
-    }
-
-    const handlerDicrement = () => {
-        setCount(count-1)
-    }
-
-
-        return (
-            <div style={{margin: 'auto',width: '300px'}} className="App"
-            >
-                <button onClick={handlerDicrement}>Минус</button>
-                <b style={countStyle}>{count}</b>
-                <button onClick={handlerIncrement}>Плюс</button>
-            </div>
-        );
+    return (
+        <>
+            <Clicker/>
+        </>
+    );
 
 
 }
 
-const countStyle = {margin: '0 2rem'}
+
 
 export default App;
---
+++ src/Clicker.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+
+export const Clicker = () => {
+
+    const [count, setCount] = useState(0)
+
+    const handlerIncrement = () => {
+        setCount(count + 1)
+    }
+
+    const handlerDicrement = () => {
+        setCount(count - 1)
+    }
+
+
+    return (
+        <div style={{margin: 'auto', width: '300px'}} className="App">
+            <button onClick={handlerDicrement}>Минус</button>
+            <b style={countStyle}>{count}</b>
+            <button onClick={handlerIncrement}>Плюс</button>
+        </div>
+    )
+
+}
+
+const countStyle = {margin: '0 2rem'}
