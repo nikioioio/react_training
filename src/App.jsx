cb59667380b07b5bda993be25072f67ae6ecdb00
Кедрун Никита <ked_nik@mail.ru>
2021-09-22 21:22:46 +0600

    counter через hook usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-// import {Form} from './Components/Form'
-import {FormWithRef} from './Components/FormWithRef'
+import React, {useState} from "react";
 
 
 const App = () => {
-    return(
-        <div className="App">
-            <FormWithRef/>
-        </div>
-    )
+
+    const [count,  setCount] = useState(0)
+
+    const handlerIncrement = () => {
+        setCount(count+1)
+    }
+
+    const handlerDicrement = () => {
+        setCount(count-1)
+    }
+
+
+        return (
+            <div style={{margin: 'auto',width: '300px'}} className="App"
+            >
+                <button onClick={handlerDicrement}>Минус</button>
+                <b style={countStyle}>{count}</b>
+                <button onClick={handlerIncrement}>Плюс</button>
+            </div>
+        );
+
+
 }
+
+const countStyle = {margin: '0 2rem'}
 
 export default App;
