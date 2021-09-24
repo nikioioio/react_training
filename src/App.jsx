96b8e85ed29eefc230ab061b82987ab1c9f55d16
Кедрун Никита <ked_nik@mail.ru>
2021-09-24 10:50:46 +0600

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
+import React, {useReducer} from 'react';
 
-import {Context} from './Contexts/Context'
-import { Books } from './Components/Books'
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "TEST_INCREMENT_RED":
+            return {
+                ...state,
+                r: state.r + 50
+            };
+        case "TEST_INCREMENT_BLUE":
+            return {
+                ...state,
+                b: state.b + 50
+            }
+        default:
+            return state;
+
+    }
+
+}
+
 
 const App = () => {
 
+    const [{r, g, b},dispatch] = useReducer(reducer, {r: 0, g: 50, b: 50})
 
     return (
         <>
-            <Context>
-                <Books/>
-            </Context>
+            <h1>Фраза меняющая цвет</h1>
+            <button onClick={() => dispatch({type: 'TEST_INCREMENT_RED'})}>testRedIncrement</button>
+            <button onClick={() => dispatch({type: 'TEST_INCREMENT_BLUE'})}>testBlueIncrement</button>
+
         </>
     );
 
 
 }
-
 
 
 export default App;
